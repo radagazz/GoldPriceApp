106489ce496bc5de9dbef0c62fa84e7f767af844
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2021-01-25 16:57:38 +0700

    Initial Commit

---
+++ GoldPriceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoldPriceApp
+//
+//  Created by anurak teerarattananukulchai on 25/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
